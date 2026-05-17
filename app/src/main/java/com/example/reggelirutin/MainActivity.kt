52d531d80c86d7a6966d2a59d4562b34de76411b
Levente Papai <papai.levente@gmail.com>
2026-05-17 14:00:13 +0200

    Updated AGP. Bugs. unexpected jvm signature V

--- app/src/main/java/com/example/reggelirutin/MainActivity.kt
+++ app/src/main/java/com/example/reggelirutin/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.History
-import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
