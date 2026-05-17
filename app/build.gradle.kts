719df95a2034c9e1dcff918dfcb94a8ea6d7bbaa
Levente Papai <papai.levente@gmail.com>
2026-05-17 14:46:23 +0200

    Version, Ui, Name for apk.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.reggelirutin"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 5
+        versionName = "1.0.5"
         base.archivesName.set("MorningRoutin")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
